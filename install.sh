4d17a316630a77f84a8762d027983364a27968fa
Euroblitz <57639120+Euroblitz@users.noreply.github.com>
2022-01-29 22:07:14 -0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-cd "$(xdg-user-dir HOME)"
-mkdir -p .jagex_cache_32
-mkdir -p .jagex_cache_32/runescape
-mkdir -p .jagex
-mkdir -p .jagex/runescape
-mkdir -p .jagex/runescape/bin
-mkdir -p .jagex/runescape/images
+cd
+mkdir -p .jagex_cache_32/runescape .jagex/runescape/{bin,icons}
 
 cd .jagex/runescape/bin
 wget https://github.com/Euroblitz/osrs-installer/blob/main/jagexappletviewer.jar
-
-cd && cd .jagex/runescape/images
+cd ../icons
 wget https://github.com/Euroblitz/osrs-installer/raw/main/jagexappletviewer.png
 
 cd "$(xdg-user-dir DESKTOP)"
+read -p "Create a desktop shortcut? (y/n)? " choice
+case "$choice" in
+  y|Y ) wget https://github.com/Euroblitz/osrs-installer/raw/main/osrs.desktop && \
+        chmod +x osrs.desktop;;
+esac
+
+read -p "Create an application menu shortcut (y/n)? " choice
+case "$choice" in 
+  y|Y ) cd ${XDG_DATA_DIR:-$HOME/.local/share}/applications && \
+        wget https://github.com/Euroblitz/osrs-installer/raw/main/osrs.desktop && \
+        chmod +x osrs.desktop;;
+esac
